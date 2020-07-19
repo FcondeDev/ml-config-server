3dcfda7526b090027ff82b0acfb0483e9ee0764b
a <a>
2020-07-19 17:15:24 -0500

    initial commit

---
+++ src/test/java/com/ml/config/MlConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
